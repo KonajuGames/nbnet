009e34828406ebdd46d662cbb178a9f5b9be907d
Nathan BIAGINI <nathan.open@gmail.com>
2021-08-30 18:49:25 +0200

    some testing code

--- rnet/test/client.c
+++ rnet/test/client.c
@@ -1,8 +1,46 @@
+#include <stdbool.h>
+
 #include "../rnet.h"
 
 int main(void)
 {
+    bool disconnected = false;
+
     StartClient("test-rnet", "127.0.0.1", 42042);
+
+    InitWindow(640, 480, "client");
+
+    while (!WindowShouldClose() && !disconnected)
+    {
+        ClientEvent ev;
+
+        AddClientTime(GetFrameTime());
+
+        while ((ev = PollClient()) != NO_EVENT)
+        {
+            if (ev == CONNECTED)
+            {
+                TraceLog(LOG_INFO, "Connected");
+            }
+            else if (ev == DISCONNECTED)
+            {
+                disconnected = true;
+            }
+        }
+
+        if (IsKeyPressed(KEY_SPACE))
+        {
+            const char *msg = "Hello world!";
+
+            SendReliableMessage((uint8_t *)msg, strlen(msg) + 1);
+        }
+
+        FlushClient();
+
+        BeginDrawing();
+            ClearBackground(LIGHTGRAY);
+        EndDrawing();
+    }
 
     StopClient();
 
--- rnet/test/server.c
+++ rnet/test/server.c
@@ -1,10 +1,68 @@
+#ifndef __EMSCRIPTEN__
+
+#include <time.h>
+
+#endif
+
 #include "../rnet.h"
+
+#define DT (1.f / 60) // 60 times per second
+
+static void Sleep(void);
 
 int main(void)
 {
     StartServer("test-rnet", 42042);
 
+    while (true)
+    {
+        AddServerTime(DT); // call once every frame
+
+        ServerEvent ev;
+
+        // poll all server events
+        while ((ev = PollServer()) != NO_EVENT)
+        {
+            if (ev == CLIENT_CONNECTION_REQUEST)
+            {
+                Connection *conn = AcceptClient();
+
+                TraceLog(LOG_INFO, "Client connected (ID: %d)", conn->id);
+            }
+            else if (ev == CLIENT_DISCONNECTED)
+            {
+                TraceLog(LOG_INFO, "Client disconnected (ID: %d)", GetDisconnectedClient()->id);
+            }
+            else if (ev == CLIENT_MESSAGE_RECEIVED)
+            {
+                Message msg;
+
+                ReadReceivedClientMessage(&msg);
+
+                TraceLog(LOG_INFO, "Received data: %s (length: %d) from client %d",
+                    msg.bytes, msg.length, msg.sender->id);
+            }
+        }
+
+        FlushServer(); // call once every frame
+
+        // sleep for a bit...
+        Sleep();
+    }
+
     StopServer();
 
     return 0;
+}
+
+static void Sleep(void)
+{
+#ifdef __EMSCRIPTEN__
+    emscripten_sleep(DT * 1000);
+#else
+    long nanos = DT * 1e9;
+    struct timespec t = {.tv_sec = nanos / 999999999, .tv_nsec = nanos % 999999999};
+
+    nanosleep(&t, &t);
+#endif
 }
